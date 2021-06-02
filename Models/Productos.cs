4db7fc3685ba06ef38bf7d6a885f1199150207a1
JerySiri <jeryjavier@hotmail.com>
2021-06-02 18:25:27 -0400

    Creacion de la entidad Productos en la carpeta Models

---
+++ Models/Productos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial1_aplicada2_2017_0826.Models
+{
+    public class Productos
+    {
+        [Key]
+        public int ProductoId { get; set; }
+
+        [Required(ErrorMessage = " Este campo de descripción no puede estar vacio")]
+        public String Descripcion { get; set; }
+
+        [Required(ErrorMessage = " Este campo de Existencia no puede estar vacio")]
+        public String Existencia { get; set; }
+
+        public float Costos { get; set; } = 0;
+
+        public int ValorInventario { get; set; } = 0;
+
+    }
+}
