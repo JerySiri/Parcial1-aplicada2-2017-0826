217a296b070462b62b6e39e0037952a95ba86b54
JerySiri <jeryjavier@hotmail.com>
2021-06-02 02:59:40 -0400

    Contexto pre-diseñado

---
+++ DAL/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial1_aplicada2_2017_0826.DAL
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source= Data\Registros.db");
+        }
+    }
+}
