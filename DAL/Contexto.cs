85aa188e32005e63d4fff928775fa4a3e4cb284b
JerySiri <jeryjavier@hotmail.com>
2021-06-02 18:33:32 -0400

    Creacion del producto en la clase contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -3,14 +3,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Parcial1_aplicada2_2017_0826.Models;
 
 namespace Parcial1_aplicada2_2017_0826.DAL
 {
     public class Contexto : DbContext
     {
+        public DbSet<Productos> Productos {get; set;}
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-
             optionsBuilder.UseSqlite(@"Data Source= Data\Articulos.db");
         }
     }
