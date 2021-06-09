7584c77b790349ff0c97ad7a5e053de8cb03f267
JerySiri <jeryjavier@hotmail.com>
2021-06-09 16:54:52 -0400

    Actualizacion de consulta

--- BLL/ProductosBLL.cs
+++ BLL/ProductosBLL.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Parcial1_aplicada2_2017_0826.Models;
 using Parcial1_aplicada2_2017_0826.DAL;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace Parcial1_aplicada2_2017_0826.BLL
 {
@@ -140,6 +141,25 @@ namespace Parcial1_aplicada2_2017_0826.BLL
 
             return paso;
 
+        }
+        public static List<Productos> GetList(Expression<Func<Productos, bool>> criterio)
+        {
+            List<Productos> lista = new List<Productos>();
+            Contexto contexto = new Contexto();
+            try
+            {
+                //obtener la lista y filtrarla según el criterio recibido por parametro.
+                lista = contexto.Productos.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
         }
     }
 }
